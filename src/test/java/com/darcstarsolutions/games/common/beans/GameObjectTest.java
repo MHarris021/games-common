6791626d3270b3801bff85d2ca5a6e688cf973b2
mharris021 <mharris021@hotmail.com>
2014-08-01 15:55:57 -0500

    Refactoring Tests to make them easier to understand Adding File JSON Deserialization Tests and Resources

--- src/test/java/com/darcstarsolutions/games/common/beans/GameObjectTest.java
+++ src/test/java/com/darcstarsolutions/games/common/beans/GameObjectTest.java
@@ -1,9 +1,13 @@
 package com.darcstarsolutions.games.common.beans;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.math.BigInteger;
 
 import static org.hamcrest.core.Is.is;
@@ -13,105 +17,137 @@ import static org.hamcrest.core.IsSame.sameInstance;
 import static org.junit.Assert.*;
 
 public class GameObjectTest {
-	private GameObject gameObject;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameObjectTest.class);
+    private GameObject gameObject;
+    private ObjectMapper objectMapper;
 
-	@Before
-	public void setUp() throws Exception {
-		gameObject = new GameObject();
-	}
+    @Before
+    public void setUp() throws Exception {
+        gameObject = new GameObject();
+        objectMapper = new ObjectMapper();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		gameObject = null;
-	}
+    @After
+    public void tearDown() throws Exception {
+        gameObject = null;
+        objectMapper = null;
+    }
 
-	@Test
-	public void testHashCode() {
-		assertNotNull(gameObject);
-		assertEquals(0, gameObject.hashCode());
-	}
+    @Test
+    public void testHashCode() {
+        assertNotNull(gameObject);
+        assertEquals(0, gameObject.hashCode());
+    }
 
-	@Test
-	public void testGameObject() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
-		assertThat(gameObject.getName(), is(equalTo("")));
-		assertThat(gameObject.getDescription(), is(equalTo("")));
-	}
+    @Test
+    public void testGameObject() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(gameObject.getName(), is(equalTo("")));
+        assertThat(gameObject.getDescription(), is(equalTo("")));
+    }
 
-	@Test
-	public void testGameObjectStringString() {
-		GameObject gameObject2 = new GameObject("test", "test description");
-		assertNotNull(gameObject2);
-		assertThat(gameObject2.getId(), is(equalTo(BigInteger.ZERO)));
-		assertThat(gameObject2.getName(), is(equalTo("test")));
-		assertThat(gameObject2.getDescription(),
-				is(equalTo("test description")));
-	}
+    @Test
+    public void testGameObjectStringString() {
+        GameObject gameObject2 = new GameObject("test", "test description");
+        assertNotNull(gameObject2);
+        assertThat(gameObject2.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(gameObject2.getName(), is(equalTo("test")));
+        assertThat(gameObject2.getDescription(),
+                is(equalTo("test description")));
+    }
 
-	@Test
-	public void testGetId() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
-	}
+    @Test
+    public void testGetId() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
+    }
 
-	@Test
-	public void testSetId() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
-		gameObject.setId(new BigInteger("100"));
-		assertThat(gameObject.getId(), is(equalTo(BigInteger.valueOf(100))));
-	}
+    @Test
+    public void testSetId() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getId(), is(equalTo(BigInteger.ZERO)));
+        gameObject.setId(new BigInteger("100"));
+        assertThat(gameObject.getId(), is(equalTo(BigInteger.valueOf(100))));
+    }
 
-	@Test
-	public void testGetName() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getName(), is(equalTo("")));
-	}
+    @Test
+    public void testGetName() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getName(), is(equalTo("")));
+    }
 
-	@Test
-	public void testSetName() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getName(), is(equalTo("")));
-		gameObject.setName("test");
-		assertThat(gameObject.getName(), is(equalTo("test")));
-	}
+    @Test
+    public void testSetName() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getName(), is(equalTo("")));
+        gameObject.setName("test");
+        assertThat(gameObject.getName(), is(equalTo("test")));
+    }
 
-	@Test
-	public void testGetDescription() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getDescription(), is(equalTo("")));
-	}
+    @Test
+    public void testGetDescription() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getDescription(), is(equalTo("")));
+    }
 
-	@Test
-	public void testSetDescription() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.getDescription(), is(equalTo("")));
-		gameObject.setDescription("test description");
-		assertThat(gameObject.getDescription(), is(equalTo("test description")));
+    @Test
+    public void testSetDescription() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.getDescription(), is(equalTo("")));
+        gameObject.setDescription("test description");
+        assertThat(gameObject.getDescription(), is(equalTo("test description")));
 
-	}
+    }
 
-	@Test
-	public void testToString() {
-		assertNotNull(gameObject);
-		assertThat(gameObject.toString(),
-				is(equalTo("GameObject{id=0, name='', description=''}")));
-	}
+    @Test
+    public void testToString() {
+        assertNotNull(gameObject);
+        assertThat(gameObject.toString(),
+                is(equalTo("GameObject{id=0, name='', description=''}")));
+    }
 
-	@Test
-	public void testCompareTo() {
-		GameObject gameObject2 = new GameObject("test", "test description");
-		gameObject2.setId(BigInteger.ONE);
-		assertThat(gameObject.compareTo(gameObject2), is(not(0)));
-	}
+    @Test
+    public void testCompareTo() {
+        GameObject gameObject2 = new GameObject("test", "test description");
+        gameObject2.setId(BigInteger.ONE);
+        assertThat(gameObject.compareTo(gameObject2), is(not(0)));
+    }
 
-	@Test
-	public void testEqualsObject() {
-		GameObject gameObject2 = new GameObject("", "");
-		gameObject2.setId(BigInteger.ZERO);
-		assertThat(gameObject.equals(gameObject2), is(true));
-		assertThat(gameObject, is(not(sameInstance(gameObject2))));
-	}
+    @Test
+    public void testEqualsObject() {
+        GameObject gameObject2 = new GameObject("", "");
+        gameObject2.setId(BigInteger.ZERO);
+        assertThat(gameObject.equals(gameObject2), is(true));
+        assertThat(gameObject, is(not(sameInstance(gameObject2))));
+    }
 
+    @Test
+    public void testGameObjectJSONSerialization() throws Exception {
+        assertTrue(objectMapper.canSerialize(GameObject.class));
+        String jsonValue = objectMapper.writeValueAsString(gameObject);
+        assertNotNull(jsonValue);
+        LOGGER.info("GameObject JSON Encoding is: {}", jsonValue);
+    }
+
+    @Test
+    public void testGameObjectJSONDeserialization() throws Exception {
+        String jsonValue = "{\"id\":0,\"name\":\"\",\"description\":\"\"}";
+        GameObject gameObjectTest = objectMapper.readValue(jsonValue, GameObject.class);
+        assertNotNull(gameObjectTest);
+        assertThat(gameObjectTest.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(gameObjectTest.getName(), is(equalTo("")));
+        assertThat(gameObjectTest.getDescription(), is(equalTo("")));
+        LOGGER.info("GameObjectTest is: {}", gameObjectTest);
+    }
+
+    @Test
+    public void testGameObjectFileJSONDeserialization() throws Exception {
+        GameObject gameObjectTest = objectMapper.readValue(new File("src/test/resources/defaultGameObject.json"), GameObject.class);
+        assertNotNull(gameObjectTest);
+        assertThat(gameObjectTest.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(gameObjectTest.getName(), is(equalTo("")));
+        assertThat(gameObjectTest.getDescription(), is(equalTo("")));
+        LOGGER.info("GameObjectTest is: {}", gameObjectTest);
+    }
 }
--- src/test/java/com/darcstarsolutions/games/common/beans/rules/GameRuleTest.java
+++ src/test/java/com/darcstarsolutions/games/common/beans/rules/GameRuleTest.java
@@ -1,52 +1,47 @@
 package com.darcstarsolutions.games.common.beans.rules;
 
-import com.darcstarsolutions.games.common.beans.Game;
-import com.darcstarsolutions.games.common.beans.Player;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.io.File;
+import java.math.BigInteger;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class GameRuleTest {
+
+    public static final String TEST = "test";
+    public static final String TEST_DESCRIPTION = "test description";
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameRuleTest.class);
+    private ObjectMapper objectMapper;
+
     private GameRule gameRule;
+
+    private GameRule defaultGameRule;
 
     @Before
     public void setUp() throws Exception {
-        new Game() {
-
-            /**
-             *
-             */
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            protected List<Player> determineWinningPlayers() {
-                return null;
-            }
-        };
+        objectMapper = new ObjectMapper();
+        defaultGameRule = new DefaultGameRule();
     }
 
     @After
     public void tearDown() throws Exception {
+        objectMapper = null;
         gameRule = null;
+        defaultGameRule = null;
     }
 
     @SuppressWarnings("serial")
     @Test
     public void testGameRule() {
-        gameRule = new GameRule() {
-
-            @Override
-            protected void applyRule() {
-
-            }
-        };
+        gameRule = new DefaultGameRule();
         assertNotNull(gameRule);
         assertThat(gameRule.getName(), is(equalTo("")));
         assertThat(gameRule.getDescription(), is(equalTo("")));
@@ -55,16 +50,55 @@ public class GameRuleTest {
     @SuppressWarnings("serial")
     @Test
     public void testGameRuleStringString() {
-        gameRule = new GameRule("test", "test description") {
-
-            @Override
-            protected void applyRule() {
-
-            }
-        };
+        gameRule = new DefaultGameRule(TEST, TEST_DESCRIPTION);
         assertNotNull(gameRule);
-        assertThat(gameRule.getName(), is(equalTo("test")));
-        assertThat(gameRule.getDescription(), is(equalTo("test description")));
+        assertThat(gameRule.getName(), is(equalTo(TEST)));
+        assertThat(gameRule.getDescription(), is(equalTo(TEST_DESCRIPTION)));
+    }
+
+    @Test
+    public void testGameRuleJSONSerialization() throws Exception {
+        assertTrue(objectMapper.canSerialize(GameRule.class));
+        assertNotNull(defaultGameRule);
+        String jsonValue = objectMapper.writeValueAsString(defaultGameRule);
+        assertNotNull(jsonValue);
+        LOGGER.info("DefaultGameRule JSON Encoding is: {}", jsonValue);
+    }
+
+    @Test
+    public void testGameRuleJSONDeserialization() throws Exception {
+        String jsonValue = "{\"class\":\"com.darcstarsolutions.games.common.beans.rules.GameRuleTest$DefaultGameRule\",\"id\":0,\"name\":\"\",\"description\":\"\"}";
+        DefaultGameRule defaultGameRuleTest = objectMapper.readValue(jsonValue, DefaultGameRule.class);
+        assertNotNull(defaultGameRuleTest);
+        assertThat(defaultGameRuleTest.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(defaultGameRuleTest.getName(), is(equalTo("")));
+        assertThat(defaultGameRuleTest.getDescription(), is(equalTo("")));
+        LOGGER.info("DefaultGameRule Test is: {}", defaultGameRuleTest);
+    }
+
+    @Test
+    public void testGameRuleFileJSONDeserialization() throws Exception {
+        DefaultGameRule defaultGameRuleTest = objectMapper.readValue(new File("src/test/resources/rules/defaultGameRule.json"), DefaultGameRule.class);
+        assertNotNull(defaultGameRuleTest);
+        assertThat(defaultGameRuleTest.getId(), is(equalTo(BigInteger.ZERO)));
+        assertThat(defaultGameRuleTest.getName(), is(equalTo("")));
+        assertThat(defaultGameRuleTest.getDescription(), is(equalTo("")));
+        LOGGER.info("DefaultGameRule Test is: {}", defaultGameRuleTest);
+    }
+
+    public static class DefaultGameRule extends GameRule {
+
+        public DefaultGameRule() {
+        }
+
+        public DefaultGameRule(String name, String description) {
+            super(name, description);
+        }
+
+        @Override
+        protected void applyRule() {
+
+        }
     }
 
 }
